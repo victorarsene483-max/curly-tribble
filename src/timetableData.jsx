86785dc43ef5211666ba452992a01f98f21903fa
unknown <victorarsene483@gmail.com>
2026-09-25 14:47:21 +0300

    Updated Timetable

--- src/timetableData.jsx
+++ src/timetableData.jsx
@@ -4,6 +4,6 @@ export const TIMETABLE = [
   { day: "Tuesday",   start: "4:00 PM", end: "7:00 PM",  code: "COSC 332", room: "BSL 503" },
   { day: "Wednesday", start: "7:00 AM",  end: "10:00 AM", code: "COSC 340", room: "BSL 203" },
   { day: "Wednesday", start: "4:00 PM", end: "7:00 PM",  code: "COSC 312", room: "FTC 002" },
-  { day: "Friday",    start: "7:00 AM",  end: "10:00 AM", code: "COSC 380", room: "BSR 403" },
+  { day: "Friday",    start: "10:00 AM",  end: "1:00 PM", code: "COSC 380", room: "BSR 403" },
   { day: "Friday",    start: "4:00 PM", end: "7:00 PM",  code: "COSC 361", room: "FTC 303" },
 ];
